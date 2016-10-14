9bbfb8431042b81212a931c22f71210770e3f040
alumno <alumno@HGOOSXCITLA06.local>
2016-10-14 09:57:13 -0500

    *con boostrap

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,6 +19,14 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('vendor/css/bootstrap-theme.min.css');
+  app.import('vendor/css/bootstrap.min.css');
+  app.import('vendor/css/bootstrap-datetimepicker.min.css');
+
+  //js
+  app.import('vendor/js/bootstrap.min.Js');
+  app.import('vendor/js/moment.js');
+  app.import('vendor/js/bootstrap-datetimepicker.min.js');
 
   return app.toTree();
 };
