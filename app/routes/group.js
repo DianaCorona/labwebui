12148961f8a956a3773020a99f5965d8fa020345
Laser <lasesabanero123@hotmail.com>
2016-10-28 13:43:11 -0500

    prueba 123

--- app/routes/group.js
+++ app/routes/group.js
@@ -1,17 +1,15 @@
 import Ember from 'ember';
 
-
-
 export default Ember.Route.extend({
-let ajax: Ember.inject.service('ajax');
-
-model:function(parametro){
-
-  let promise= this.get('ajax').request('http://localhost:3000/users');
-    //ember esperara a que se complete la promise
-    return promise;
-
-}
+// ajax: Ember.inject.service('ajax');
+//
+// // model:function(parametro){
+// //
+// //   // let promise= this.get('ajax').request('http://localhost:3000/users');
+// //   //   //ember esperara a que se complete la promise
+// //   //   return promise;
+// //
+// // }
 
 
 
