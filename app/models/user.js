53041379e815a957a0e9b9e34ceb6d2ff1a58631
DianaCorona <A01273540@itesm.mx>
2016-10-31 20:14:16 -0600

    *No se que sucede!!! Ayuda :)

---
+++ app/models/user.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+	nickname: DS.attr('string'),
+	name: DS.attr('string'),
+	lastName: DS.attr('string'),
+	birthday: DS.attr('date'),
+	phone: DS.attr('string'),
+	email: DS.attr('string'),
+    password: DS.attr('string')
+});
--- app/routes/create-user.js
+++ app/routes/create-user.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model () {
+		return this.store.createRecord('user');
+	},
+	action:{
+		createUser(model){
+			model.save().then(()=>{
+
+			}).cath(()=>{
+
+			});
+		}
+	}
 });
