f6c13e9e1236ea21247c3819e5289ad95ba357ea
DianaCorona <A01273540@itesm.mx>
2016-11-01 15:16:47 -0600

    *ya actualiza

--- app/adapters/user.js
+++ app/adapters/user.js
@@ -3,6 +3,9 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 export default RESTAdapter.extend(DataAdapterMixin,{
 	authorizer: 'authorizer:devise',
 	urlForCreateRecord(){
-		return "/users/create";
+		return `/users/create`;
+	}, 
+	urlForUpdateRecord(id){
+		return `/users/${id}/update`
 	}
 });
