2915128f51362773c1dc4820765c5db1ad42775a
Laser <lasesabanero123@hotmail.com>
2016-11-02 00:27:38 -0600

    Version 2 Parcial Vistas usables Create User, login, Create Group sigue en proceso algunas vistas, API rails ya es operable

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,6 +1,15 @@
 import Ember from 'ember';
+import inject from 'ember-service/inject';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(AuthenticatedRouteMixin,{
+	session: inject(),
 	tagName: 'nav',
-	classNames: "navbar navbar-default"	
+	classNames: "navbar navbar-default",
+
+	actions: {
+		logout() {
+			this.get('session').invalidate();
+		}
+	},
 });
