d62b9045b63b4ccc2b5885d368d5411f2e786efd
Laser <lasesabanero123@hotmail.com>
2016-12-01 19:16:13 -0600

    Cambios por luis ;D

--- app/models/listg.js
+++ app/models/listg.js
@@ -4,5 +4,5 @@ export default DS.Model.extend({
   // user_id: DS.attr('string'),
   // group_id: DS.attr('string'),
   name: DS.attr('string'),
-  expDate: DS.attr('string'),
+  exp_date: DS.attr('string'),
 });
